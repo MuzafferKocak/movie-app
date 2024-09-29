e5ae91c3148a568428ebb06209a3e8a105cdf287
Mahir <mkojak75@gmail.com>
2024-09-30 01:03:32 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Register from "./pages/Register";
 
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <div className="App">
-      <Register />
+    <div className="dark:bg-gray-dark-main min-h-screen">
+      <AppRouter />
     </div>
   );
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/detail/:id" element={<MovieDetail />} />
+      </Routes>
+    </>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
